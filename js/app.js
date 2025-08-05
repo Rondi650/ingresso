19191c427594a32be4376afb7bc0b92dc2ecb1cf
Rondi650 <rondinellenunes123@gmail.com>
2025-08-05 20:52:28 -0300

    Primeiro commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+function comprar() {
+    let tipoDeIngresso = document.getElementById("tipo-ingresso").value
+    let quantidade = parseInt(document.getElementById('qtd').value)
+
+    if (tipoDeIngresso == 'pista') {
+        pista(quantidade);
+    } else if (tipoDeIngresso == 'inferior') {
+        inferior(quantidade);
+    } else {
+        superior(quantidade);
+    }
+
+}
+
+function pista(qtd) {
+    let valor = parseInt(document.getElementById("qtd-pista").innerHTML)
+    if (qtd > valor) {
+        alert("Quantidade superior ao disponivel em estoque")
+    } else {
+        alert('Compra Realizada com Sucesso!')
+        let resultado = (valor - qtd)
+        document.getElementById("qtd-pista").innerHTML = resultado
+    }
+}
+
+function inferior(qtd) {
+    let valor = parseInt(document.getElementById("qtd-inferior").innerHTML)
+    if (qtd > valor) {
+        alert("Quantidade superior ao disponivel em estoque")
+    } else {
+        alert('Compra Realizada com Sucesso!')
+        let resultado = (valor - qtd)
+        document.getElementById("qtd-inferior").innerHTML = resultado
+    }
+}
+
+function superior(qtd) {
+    let valor = parseInt(document.getElementById("qtd-superior").innerHTML)
+    if (qtd > valor) {
+        alert("Quantidade superior ao disponivel em estoque")
+    } else {
+        alert('Compra Realizada com Sucesso!')
+        let resultado = (valor - qtd)
+        document.getElementById("qtd-superior").innerHTML = resultado
+    }
+}
